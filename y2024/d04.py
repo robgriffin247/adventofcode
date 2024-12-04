75efd03deb151ecd8be692b4d9a9c83ae67548aa
robgriffin247 <robgriffin247@hotmail.com>
2024-12-05 00:00:56 +0100

    ugly day 4 done

--- y2024/d04.py
+++ y2024/d04.py
@@ -14,18 +14,18 @@ def part1(data=data):
 
     count = 0
 
-    for x in range(matrix.shape[0]-3):#range(137):
-        for y in range(matrix.shape[1]-3):# range(137):
+    for x in range(matrix.shape[0]-3):
+        for y in range(matrix.shape[1]-3):
             sub_matrix = matrix[np.ix_([x,x+1,x+2,x+3], [y,y+1,y+2,y+3])]
             lines = [
-                sub_matrix[np.ix_([0], [0,1,2,3])].tostring().decode().replace("\x00", ""), # left line
-                sub_matrix[np.ix_([0], [0,1,2,3])].tostring().decode().replace("\x00", "")[::-1], # left line rev
-                sub_matrix[np.ix_([0,1,2,3], [0])].tostring().decode().replace("\x00", ""), # top line
-                sub_matrix[np.ix_([0,1,2,3], [0])].tostring().decode().replace("\x00", "")[::-1], # top line rev
-                np.diag(sub_matrix).tostring().decode().replace("\x00", ""), # 0,0 to 3,3
-                np.diag(np.rot90(sub_matrix, 1)).tostring().decode().replace("\x00", ""), # 0,3 to 3,0
-                np.diag(np.rot90(sub_matrix, 2)).tostring().decode().replace("\x00", ""), # 0,3 to 3,0
-                np.diag(np.rot90(sub_matrix, 3)).tostring().decode().replace("\x00", ""), # 0,3 to 3,0
+                sub_matrix[np.ix_([0], [0,1,2,3])].tostring().decode().replace("\x00", ""), 
+                sub_matrix[np.ix_([0], [0,1,2,3])].tostring().decode().replace("\x00", "")[::-1],
+                sub_matrix[np.ix_([0,1,2,3], [0])].tostring().decode().replace("\x00", ""),
+                sub_matrix[np.ix_([0,1,2,3], [0])].tostring().decode().replace("\x00", "")[::-1],
+                np.diag(sub_matrix).tostring().decode().replace("\x00", ""),
+                np.diag(np.rot90(sub_matrix, 1)).tostring().decode().replace("\x00", ""),
+                np.diag(np.rot90(sub_matrix, 2)).tostring().decode().replace("\x00", ""),
+                np.diag(np.rot90(sub_matrix, 3)).tostring().decode().replace("\x00", ""),
                 ]
             
             if x+4==matrix.shape[0]:
@@ -48,4 +48,21 @@ def part1(data=data):
     return count
 
 def part2(data=data):
-    return "incomplete"
+
+    split_data = data.replace('\\', "").split("n")
+    letters = [list(i) for i in split_data]
+    matrix = np.matrix(letters)
+
+    count = 0
+
+    for x in range(matrix.shape[0]-2):
+        for y in range(matrix.shape[1]-2):
+            sub_matrix = matrix[np.ix_([x,x+1,x+2], [y,y+1,y+2])]
+            lines = [
+                np.diag(sub_matrix).tostring().decode().replace("\x00", ""),
+                np.diag(np.rot90(sub_matrix, 1)).tostring().decode().replace("\x00", ""),
+                np.diag(np.rot90(sub_matrix, 2)).tostring().decode().replace("\x00", ""),
+                np.diag(np.rot90(sub_matrix, 3)).tostring().decode().replace("\x00", ""),
+                ]
+            count += sum([l=="MAS" for l in lines])==2
+    return count
